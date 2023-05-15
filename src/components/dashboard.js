9562eb08345b148a1f40e005a3f82e2a62bca3b5
DESKTOP-AI6RRTR\user <jiyumi00@naver.com>
2023-05-15 15:26:14 +0900

    캘린더 추가

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import FullCalendar from "@fullcalendar/react";
+import FullCalendar from '@fullcalendar/react';
 
 export default class DashBoard extends Component {
     constructor(props){
@@ -14,20 +14,21 @@ export default class DashBoard extends Component {
     render() {
         return(
             <div>
-            메인 화면입니다.
-            <FullCalendar
-                plugins={[dayGridPlugin,timeGridPlugin,interactionPlugin]}
-                initialDate={'dayGridMonth'}
-                headerToolbar={
-                    {
-                        start: 'today prev,next', // will normally be on the left. if RTL, will be on the right
-                        center: 'title',
-                        end: 'dayGridMonth,timeGridWeek,timeGridDay' // will normally be on the right. if RTL, will be on the left
+            <FullCalendar   
+                    plugins={[dayGridPlugin,timeGridPlugin,interactionPlugin]}
+                    initialView={'dayGridMonth'}
+                    headerToolbar={
+                        {
+                            start: 'today prev,next', // will normally be on the left. if RTL, will be on the right
+                            center: 'title',
+                            end: 'dayGridMonth,timeGridWeek,timeGridDay' // will normally be on the right. if RTL, will be on the left
+                        }
                     }
-                }
-            />
-
-
+                
+                    height={"70vh"}
+                    dateClick={this.dateClick}
+                    events={[{title:'판매건수 : 23건', date:'2023-05-11',},{title:'매출액 : 2000원',date:'2023-05-11',}]}
+                />
             </div>
         );
     }
